ce2cf7dcc20c61a9b2095f66f3ce646de29029a6
csjin7 <csjin72@gmail.com>
2017-12-26 02:26:50 +0900

    local commit chap02 pushToUpstreams

---
+++ chap02/src/chap02/PushToUpstreams.java
@@ -0,0 +1,10 @@
+package chap02;
+
+public class PushToUpstreams {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("PushToUpstreams");
+	}
+
+}
